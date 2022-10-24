ca5af9c1a5f87581946e4e140c04f03583b04f4c
Melisa Burlando <melisa.burlando@gmail.com>
2022-10-24 00:25:49 -0300

    Agrego ejercicios del practico 4

---
+++ practicos/practico4/index.php
@@ -0,0 +1,35 @@
+<?php
+
+echo('
+    <h1> Práctico 4 </h1>
+    <h2> Ejercicio 1 </h2>
+    <h3> Qué es MVC y qué ventajas tiene? </h3>
+    <p> Modelo, Vista y Controlador, La ventaja es que queda todo mejor organizado y es mucho más facil encontrar las cosas 
+    y el díade mañana si hay que cambiar algo no hay que estar tocando todo el codigo </p>
+    <p> </p>
+
+    <h1> Práctico 4 </h1>
+    <h2> Ejercicio 2 </h2>
+    <h3>Verdadero o falso y justifique</h3>
+    <p> a) En MVC el modelo es el intermediario entre la vista y el controlador </p>
+    <p> FALSO, es el controlador el que hace de intermediario, el modelo nunca tiene comunicacion directa con la vista</p>
+    <p> b) el controlador es el encargado de capturar los eventos del usuario </p>
+    <p> En realidad lo hace el router, pero de ahí van al controlador y es quien decide si llamar a la vista o al modelo segun corresponda</p>
+    <p> c) Las reglas de negocio se implementan en el modelo </p>
+    <p> No entendi la afirmación..... Pero... no... el modelo solo trae los datos de la tabla de datos. </p>
+    
+    <h1> Práctico 4 </h1>
+    <h2> Ejercicio 3 </h2>
+    <h3> En qué casos no usaría MVC para desarrollar una app </h3>
+    <p> No sé.... </p>
+    <p> </p>
+
+    <h1> Práctico 4 </h1>
+    <h2> Ejercicio 4 </h2>
+    <h3></h3>
+    <p> </p>
+    <p> b) Ingrese algunos valores de ejemplo directamente desde un ascript php (sin form) </p>
+    <p> c)form para ingresar pagos </p>
+    <p> d)Como no registrar dos veces el mismo pago??? </p>
+    <p> con un número de factura </p>
+');
