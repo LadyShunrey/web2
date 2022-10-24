a25dfc3032923663bca835af46db833d3ec14f1e
Melisa Burlando <melisa.burlando@gmail.com>
2022-10-24 00:11:06 -0300

    Agrego ejercicios del practico 3

---
+++ practicos/practico3/index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo('
+    <h1> Práctico 3 </h1>
+    <h2> Ejercicio 1 </h2>
+    <h3> Ejemplos concretos de porque se utilizan archivos en vez de una BBDD </h3>
+    <p> No entiendo la pregunta </p>
+    <p> porue es lo clásico y es poco costoso pero tiene muchas desventajas </p>
+
+    <h1> Práctico 3 </h1>
+    <h2> Ejercicio 2 </h2>
+    <h3> Responda y justifique </h3>
+    <p> a)Las imágenes y videos, se pueden almacenar en una BBDD? </p>
+    <p> No! En realidad si se puede pero bueno vamos a decir que no.</p>
+    <p> b)Cómo piensa que se puede tener asociada una imagen a un registro (por ejemplo, la foto carnet de un usuario?) </p>
+    <p> Poniendo la imagen en nuestro repo y guardando en la base de datos la ruta de la imagen! </p>
+    
+    <h1> Práctico 3 </h1>
+    <h2> Ejercicio 3 </h2>
+    <h3> Crear una bd para registrar pagos de deudas en phpMyAdmin, SIN USAR PHP </h3>
+    <p> Es necesario agregar una clave primaria? </p>
+    <p> Sí! Un campo ID que funcione como primary key, porque puede suceder que haya dos usuarios con el mismo nombre! </p>
+
+    <h1> Práctico 3 </h1>
+    <h2> Ejercicio 4 </h2>
+    <h3> Usando la tabla anterior desde PHP hacer:</h3>
+    <p> a) Liste todos los registros de la tabla en html </p>
+    <p> b) Ingrese algunos valores de ejemplo directamente desde un ascript php (sin form) </p>
+    <p> c)form para ingresar pagos </p>
+    <p> d)Como no registrar dos veces el mismo pago??? </p>
+    <p> con un número de factura </p>
+');
